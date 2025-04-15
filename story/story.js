00ca46fa0d3c1268ca3b38588213d83781527e7f
leo14208 <sucongxiang86@gmail.com>
2025-04-15 14:55:26 +0800

    Add files via upload

---
+++ story/story.js
@@ -0,0 +1,13 @@
+<nav>
+    <a href="#scene1">Scene 1</a>
+    <a href="#scene2">Scene 2</a>
+    <a href="#scene3">Scene 3</a>
+    <a href="#scene4">Scene 4</a>
+    <a href="#scene5">Scene 5</a>
+    <a href="#scene6">Scene 6</a>
+    <a href="#scene7">Scene 7</a>
+    <a href="#scene8">Scene 8</a>
+    <a href="#scene9">Scene 9</a>
+    <a href="#scene10">Scene 10</a>
+  </nav>
+  
